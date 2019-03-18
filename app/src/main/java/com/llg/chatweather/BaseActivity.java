7425583e247f3cf65f4fc253ecca4e0ea9609f1b
leelugen <leelugen@163.com>
2019-03-18 20:58:43 +0800

    add

---
+++ app/src/main/java/com/llg/chatweather/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.llg.chatweather;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * create by loogen on 2019-3-18
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+    }
+
+    //
+    protected abstract int getLayoutId();
+}
---
+++ app/src/main/java/com/llg/chatweather/http/API.java
@@ -0,0 +1,10 @@
+package com.llg.chatweather.http;
+
+/**
+ * create by loogen on 2019-3-18
+ */
+public class API {
+    public static final String API_KEY = "gszdv59kxtnoorb3";
+
+
+}
