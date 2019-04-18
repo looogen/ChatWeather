12d55b84603f4588924587a549e478db1de5a13f
lee <leelugen@163.com>
2019-04-18 22:59:28 +0800

    解决toolbar显示问题

--- app/src/main/java/com/llg/chatweather/view/BaseActivity.java
+++ app/src/main/java/com/llg/chatweather/view/BaseActivity.java
@@ -3,7 +3,6 @@ package com.llg.chatweather.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import io.reactivex.disposables.CompositeDisposable;
 
--- app/src/main/java/com/llg/chatweather/view/fragment/MainFragment.java
+++ app/src/main/java/com/llg/chatweather/view/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -51,7 +52,9 @@ public class MainFragment extends SimpleImmersionFragment {
     }
 
     private void setUpBar() {
-
+        mActivity.setSupportActionBar(mToolbar);
+        ActionBar actionBar = mActivity.getSupportActionBar();
+        mToolbar.setNavigationIcon(R.drawable.ic_menu);
     }
 
     @Override
@@ -75,12 +78,12 @@ public class MainFragment extends SimpleImmersionFragment {
         mBindingView.setWeatherviewmodel(weatherViewModel);
     }
 
+
+
     @Override
     public void initImmersionBar() {
                 ImmersionBar.with(this)
-                .titleBar(mToolbar, true)
                 .transparentBar()
                 .init();
-
     }
 }
